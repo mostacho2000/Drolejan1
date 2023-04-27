f0fb922d5266aef7d34af090f3055895658f91c4
mostacho2000 <a22390190@ucslp.net>
2023-04-27 09:14:34 -0600

    Los odio a todos

---
+++ Assets/Enemigos/Quitar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quitar : MonoBehaviour
+{
+    public void Salir()//esto se le ayade al boton de restart
+    {
+
+
+      
+        Application.Quit();
+
+
+        
+    }
+}
